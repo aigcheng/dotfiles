73b529d535ceef72d1d7632da1992540082a67f5
czen <metaczen@gmail.com>
2022-01-02 11:32:10 +0800

    feat: clear useless config

--- alfred/Alfred.alfredpreferences/workflows/user.workflow.5DF1D281-6A98-4C22-BFEE-F3F85FC22641/focus-add.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$blocked   = json_decode(file_get_contents('blocked-sites.json'));
-$blocked[] = $query;
-
-sort($blocked);
-
-file_put_contents('blocked-sites.json', json_encode($blocked));
--- alfred/Alfred.alfredpreferences/workflows/user.workflow.B75190BE-05BA-4905-A2A9-E34EDC64EB48/README.md
+++
@@ -1,4 +0,0 @@
-# Alfred 2 Number Converter Workflow
-
-Convert numbers between binary, octagonal, decimal and hexadecimal
-
